05348476b8f91b824253809696974c383631ae8b
Guilherme Marques <guilhermemarques1@ice.ufjf.br>
2022-02-05 21:27:36 -0300

    Iniciando Pac-Man 3D

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+        transform.position = transform.position + horizontal * Time.deltaTime * 8;
+        Vector3 teste = new Vector3(0.0f, 0.0f, Input.GetAxis("Vertical"));   
+    }
+}
